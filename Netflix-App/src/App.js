02bf4332d0693a0bd566ab22a511373faaf11ccd
beejay321 <jibodu.busola@gmail.com>
2021-05-05 15:13:02 +0200

    Update

--- Netflix-App/src/App.js
+++ Netflix-App/src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SearchQuery from "./components/Search/SearchQuery";
 import MyNav from "./components/Navbar/MyNav.jsx";
+import { BrowseRouter as Router, Route } from "react-router-dom";
+import ShowDetail from "./components/ShowDetail";
 
 // import MyFooter from "./components/Footer/MyFooter";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <MyNav />
       <SearchQuery genre="action" />
+      <Route component={ShowDetail} path="/details/:movieId" />
       {/* <MyFooter /> */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ Netflix-App/src/components/ShowDetail.jsx
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+
+class ShowDetail extends Component {
+  state = {
+    MovieToShow: null,
+  };
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <>
+        <h1> DETAILS </h1>
+      </>
+    );
+  }
+}
+
+export default ShowDetail;
